3f341f329d66557707621b9679d57ffe1b06cf1b
oani <callmeoani@gmail.com>
2025-02-12 02:08:48 +0100

    adding routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
-import Layout from "./layouts/Layout";
+import { createBrowserRouter, RouterProvider } from "react-router";
+// import Layout from "./layouts/Layout";
+import PrimaryLayout from "./layouts/PrimaryLayout";
+import LandingRoutes from "./routes/LandingRoutes";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <PrimaryLayout />,
+    children: [LandingRoutes],
+  },
+]);
 function App() {
-  return <Layout></Layout>;
+  return <RouterProvider router={router} />;
 }
 export default App;
